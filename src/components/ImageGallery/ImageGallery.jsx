27c11cb106edcd8f74deec918e0367b702d68abf
conorbailey90 <conbailey90@gmail.com>
2024-02-10 22:27:45 +0000

    Images updated

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,35 +6,36 @@ function ImageGallery() {
         <div className={styles.container}>
           <div className={styles.carousel}>
             <div className={styles.carouselInner}>
+                
               <div className={styles.imageContainer} style={{width: '12.5%'}}>
                   <Image className={styles.image}  alt='amanda and quentin' src={'/images/amandaquentin1.jpeg'} fill></Image>
               </div>
               <div className={styles.imageContainer} style={{width: '10%'}} >
-                  <Image className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin2.jpeg'} fill></Image>
+                  <img className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin2.jpeg'} />
               </div>
               <div className={styles.imageContainer} style={{width: '12.5%'}} >
-                  <Image className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin3.jpeg'} fill></Image>
+                  <img className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin3.jpeg'} />
               </div>
               <div className={styles.imageContainer} style={{width: '10%'}} >
-                  <Image className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin5.jpeg'} fill></Image>
+                  <img className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin5.jpeg'} />
               </div>
               <div className={styles.imageContainer} style={{width: '7.5%'}} >
-                  <Image className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin7.jpeg'} fill></Image>
+                  <img className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin7.jpeg'} />
               </div>
               <div className={styles.imageContainer} style={{width: '12.5%'}}>
-                  <Image className={styles.image}  alt='amanda and quentin' src={'/images/amandaquentin1.jpeg'} fill></Image>
+                  <img className={styles.image}  alt='amanda and quentin' src={'/images/amandaquentin1.jpeg'} />
               </div>
               <div className={styles.imageContainer} style={{width: '10%'}} >
-                  <Image className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin2.jpeg'} fill></Image>
+                  <img className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin2.jpeg'} />
               </div>
               <div className={styles.imageContainer} style={{width: '12.5%'}} >
-                  <Image className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin3.jpeg'} fill></Image>
+                  <img className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin3.jpeg'} />
               </div>
               <div className={styles.imageContainer} style={{width: '10%'}} >
-                  <Image className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin5.jpeg'} fill></Image>
+                  <img className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin5.jpeg'} />
               </div>
               <div className={styles.imageContainer} style={{width: '7.5%'}} >
-                  <Image className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin7.jpeg'} fill></Image>
+                  <img className={styles.image} alt='amanda and quentin' src={'/images/amandaquentin7.jpeg'} />
               </div>
             </div>
           </div>
