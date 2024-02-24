fcd13150168e70163233b17ad1e3088a4ad41dd3
conorbailey90 <conbailey90@gmail.com>
2024-02-24 19:03:21 +0000

    revalidation added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import { Hero, Invitation, ImageGallery, Timeline } from "@/components/export";
 import Image from "next/image";
 import styles from "./page.module.css";
 
+export const revalidate = 30 // revalidate at most every hour
+
 export default function Home() {
   return (
     <main className={styles.main}>
